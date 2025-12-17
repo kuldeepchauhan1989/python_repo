5e51d8d49a9f4bae79a0448fd099b20e7e60ba53
kuldeepchauhan1989 <kuldeepchauhan1989@zohomail.in>
2025-12-17 23:16:53 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,7 @@
+num1 = input("Enter First Number: ")
+num2 = input("Enter Second Number: ")
 
+print("Addition: ",int(num1)+int(num2))
+print("Subtraction: ",int(num1)-int(num2))
+print("Multiplication: ",int(num1)*int(num2))
+print("Division: ",int(num1)/int(num2))
